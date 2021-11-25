1aaff503ae3439ba0a5761a0770d772bf65b0ab3
krit999 <88371603+krit999@users.noreply.github.com>
2021-11-25 13:15:11 +0700

    Add files via upload

---
+++ linepy/__init__.py
@@ -0,0 +1,15 @@
+from .client import LINE
+from .call import Call
+from .channel import Channel
+from .oepoll import OEPoll
+from akad.ttypes import OpType
+
+__copyright__       = 'Dev. 2021 by atipat witidnun'
+__version__         = '4.0.0'
+__license__         = 'BSD-3-Clause'
+__author__          = 'Atipat Witidnun'
+__author_email__    = 'MixROP@icoud.com'
+__url__             = 'https://github.com/THxMixR/BotLineThai'
+
+__all__ = ['LINE', 'Channel', 'OEPoll', 'OpType', 'Call']
+#test
